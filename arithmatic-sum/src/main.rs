88e7830ae2ab2c0bbb19188f5177ab3173f231cd
Mrigesh <mrigesh.patni@gmail.com>
2024-08-06 15:50:14 +0530

    Initial commit

--- arithmatic-sum/src/main.rs
+++ arithmatic-sum/src/main.rs
@@ -1,3 +1,6 @@
+// 'Immutable'? A variable that cannot vary? let variables by default can only be assigned a value when declared. 
+// Adding the magic word mut (please make this variable mutable) does the trick:
+
 //  sum +=i;
 //   |         ^^^^^^^ cannot assign twice to immutable variable
 // if we want to change values of variables in runtime, we need to make them mutable using mut keyword
